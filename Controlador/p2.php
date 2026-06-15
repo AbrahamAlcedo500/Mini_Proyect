5ea0b30ba11b4ac2e380191d94f80ebcc0d36b75
AbrahamAlcedo500 <pilasparce24@gmail.com>
2026-06-14 20:55:52 -0500

    reeditando

--- Controlador/p2.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../Modelo/ModeloP2.php';
-
-// Inicializamos la variable que va a heredar la vista
-$suma = ModeloP2::calcularSuma();
-
-// Llamamos a la vista del problema 2
-include '../Vista/problema2.php';
-?>
